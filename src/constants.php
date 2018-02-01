e1cca81ecf31fa3fd33ffec6ddadefe932da057a
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-02-01 13:55:36 +0000

    adding a constants file, tweaks to bash script

---
+++ src/constants.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace EdmondsCommerce\PHPQA;
+
+
+class constants
+{
+    /**
+     * The key in $_SERVER that we check for in our PHPUnit tests
+     */
+    const QA_QUICK_TESTS_KEY = 'qaQuickTests';
+
+    /**
+     * The value for the key in $_SERVER that we check for in our PHPUnit tests. If equal to this value, we can skip
+     * tests
+     */
+    const QA_QUICK_TESTS_ENABLED = 1;
+
+}
