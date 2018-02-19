3716c783ff2b5dd1528abd5784f80c01be34f69b
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-02-19 17:15:35 +0000

    dont leave the broken md file lying around;

--- docs/test.md
+++
@@ -1,6 +0,0 @@
-This is a test file for markdown linting
-
-[correct link](./../README.md) should work
-
-[incorrect link](./../nothere.md) should not work
-
--- tests/Markdown/LinksCheckerTest.php
+++ tests/Markdown/LinksCheckerTest.php
@@ -7,21 +7,44 @@ use PHPUnit\Framework\TestCase;
 
 class LinksCheckerTest extends TestCase
 {
+    private $testMdFile;
+
+    public function setup()
+    {
+        $root             = Config::getProjectRootDirectory();
+        $this->testMdFile = $root.'/docs/test.md';
+    }
+
     /**
      * @throws \Exception
      * @SuppressWarnings(PHPMD.StaticAccess)
      */
     public function testMain()
     {
-        $root     = Config::getProjectRootDirectory();
-        $expected = "\n$root".'/docs/test.md
+
+        file_put_contents($this->testMdFile, <<<MARKDOWN
+This is a test file for markdown linting
+
+[correct link](./../README.md) should work
+
+[incorrect link](./../nothere.md) should not work
+MARKDOWN
+        );
+        $expected = "\n$this->testMdFile
 ----------------------------------
 
-Bad link for "incorrect link" to "./../nothere.md"
-';
+Bad link for \"incorrect link\" to \"./../nothere.md\"
+";
         ob_start();
         LinksChecker::main();
         $actual = ob_get_clean();
         $this->assertEquals($expected, $actual);
+    }
+
+    public function __destruct()
+    {
+        if (file_exists($this->testMdFile)) {
+            unlink($this->testMdFile);
+        }
     }
 }
