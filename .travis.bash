bb1d7673f1db73f4dfb291edfe00156b3768ac6f
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-02-20 07:41:05 +0000

    travis

---
+++ .travis.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+cd $DIR;
+set -e
+set -u
+set -o pipefail
+standardIFS="$IFS"
+IFS=$'\n\t'
+echo "
+===========================================
+$(hostname) $0 $@
+===========================================
+"
+rm -f composer.lock
+gitBranch=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+echo "gitBranch is $gitBranch"
+git checkout $gitBranch
+composer install
+
+mkdir -p $DIR/cache/qa && chmod 777 $DIR/cache/Proxies
+
+echo "
+===========================================
+$(hostname) $0 $@ COMPLETED
+===========================================
+"
