a6b90692c8d1008969df8569aa505043344063cf
Ken <ken@edmondscommerce.co.uk>
2018-04-10 19:19:03 +0000

    Added MEQP incompatibility warning to codesniffer

--- includes/magento2-project/codeSniffer.inc.bash
+++ includes/magento2-project/codeSniffer.inc.bash
@@ -5,8 +5,35 @@ pathsString=$(IFS=" " eval 'echo "${pathsToCheck[*]}"')
 
 if [[ ! -d $projectRoot/vendor/magento/marketplace-eqp ]]
 then
-    echo "Make sure the MEQP standards are installed by running"
-    echo "composer require magento/marketplace-eqp"
+    echo "ERROR: The MEQP Coding Standards are not installed"
+    echo ""
+    echo "Make sure the MEQP standards are installed by adding it to Magento's composer dependencies"
+    echo ""
+    echo "** MEQP CodeSniffer Incompatibilities **"
+    echo "At the time of writing, all versioned magento/marketplace-eqp require a version"
+    echo "of CodeSniffer (2.6.2) that's incompatible with the one in Magento's requirements"
+    echo "An in-development version of MEQP resolves this, but is not yet available on Packagist"
+    echo ""
+    echo "You must add a new repository to Magento's composer.json:"
+    echo '
+"repositories": {
+    ...
+    "marketplace-eqp": {
+        "type": "vcs",
+        "url": "https://github.com/magento/marketplace-eqp"
+    }
+    ...
+}
+    '
+    echo ""
+    echo "And then require-dev the master branch:"
+    echo '
+"require-dev": {
+    ...
+    "magento/marketplace-eqp": "dev-master"
+    ...
+}
+    '
     exit 1
 fi
 
@@ -22,7 +49,9 @@ do
     set -x
     eval phpNoXdebug -f bin/phpcs -- \
         --config-set installed_paths "$projectRoot/vendor/magento/marketplace-eqp/" \
-        --config-set m2-path $projectRoot \
+        --config-set m2-path $projectRoot
+
+    eval phpNoXdebug -f bin/phpcs -- \
         --standard="MEQP2" \
         --colors \
         --cache="$cacheDir"/phpcs.cache \
