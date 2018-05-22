2cb014791df2a971733fce4ed44cc165f67a619a
Ross Mitchell <ross@edmondscommerce.co.uk>
2018-05-22 16:31:45 +0100

    Now that this is running I can remove the debugging code

--- includes/generic/infection.inc.bash
+++ includes/generic/infection.inc.bash
@@ -4,8 +4,6 @@ set -u
 set -o pipefail
 standardIFS="$IFS"
 IFS=$'\n\t'
-set -x
-
 # We need to be able to generate coverage for infection to calculate its metrics
 phpCmd=\php
 ${phpCmd} ./bin/infection \
@@ -13,5 +11,4 @@ ${phpCmd} ./bin/infection \
 --configuration=${infectionConfig} \
 --min-msi=${mutationScoreIndicator} \
 --min-covered-msi=${coveredCodeMSI}
-set +x
 
