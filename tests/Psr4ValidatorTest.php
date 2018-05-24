8a2e67a2b4feee7d95ca5052034a5c6ee111c25b
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-05-24 16:31:01 +0000

    dynamic project root in expected

--- tests/Psr4ValidatorTest.php
+++ tests/Psr4ValidatorTest.php
@@ -64,13 +64,13 @@ class Psr4ValidatorTest extends TestCase
                         [
                             0 =>
                                 [
-                                    'fileInfo'          => '/var/www/vhosts/phpqa/tests/assets/psr4/projectInValid/src/Wrong.php',
+                                    'fileInfo'          => $projectRoot.'/src/Wrong.php',
                                     'expectedNamespace' => 'In\\Valid',
                                     'actualNamespace'   => 'Totally',
                                 ],
                             1 =>
                                 [
-                                    'fileInfo'          => '/var/www/vhosts/phpqa/tests/assets/psr4/projectInValid/src/Nested/Deep/Bad.php',
+                                    'fileInfo'          => $projectRoot.'/src/Nested/Deep/Bad.php',
                                     'expectedNamespace' => 'In\\Valid\\Nested\\Deep',
                                     'actualNamespace'   => 'So',
                                 ],
@@ -78,12 +78,12 @@ class Psr4ValidatorTest extends TestCase
                 ],
             'Parse Errors:'  =>
                 [
-                    0 => '/var/www/vhosts/phpqa/tests/assets/psr4/projectInValid/tests/ParseError.php',
+                    0 => $projectRoot.'/tests/ParseError.php',
                 ],
             'Ignored Files:' =>
                 [
-                    0 => '/var/www/vhosts/phpqa/tests/assets/psr4/projectInValid/src/IgnoredStuff/Ignored.php',
-                    1 => '/var/www/vhosts/phpqa/tests/assets/psr4/projectInValid/src/IgnoredStuff/InvalidIgnored.php',
+                    0 => $projectRoot.'/src/IgnoredStuff/Ignored.php',
+                    1 => $projectRoot.'/src/IgnoredStuff/InvalidIgnored.php',
                 ],
             'Missing Paths:' =>
                 [
