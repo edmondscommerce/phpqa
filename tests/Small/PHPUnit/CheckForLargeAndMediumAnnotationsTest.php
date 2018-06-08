0ecd153d461bb02ada43f12f9b7a6c211d86ab7c
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-06-08 10:49:47 +0000

    fixing incorrect findy replacey

--- tests/Small/PHPUnit/CheckForLargeAndMediumAnnotationsTest.php
+++ tests/Small/PHPUnit/CheckForLargeAndMediumAnnotationsTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace EdmondsCommerce\PHPQA\Tests\Large\PHPUnit;
+namespace EdmondsCommerce\PHPQA\Tests\Small\PHPUnit;
 
 use EdmondsCommerce\PHPQA\PHPUnit\CheckForLargeAndMediumAnnotations;
 use PHPUnit\Framework\TestCase;
