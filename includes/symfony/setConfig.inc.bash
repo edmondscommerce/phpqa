270bb60f2489d75912735d070ec13c576e186a70
Ross Mitchell <ross@edmondsommerce.co.uk>
2019-11-08 14:09:53 +0000

    Use arrays for this so multiple directories can be handled

--- includes/symfony/setConfig.inc.bash
+++ includes/symfony/setConfig.inc.bash
@@ -1,7 +1,9 @@
 source ${DIR}/../includes/generic/setConfig.inc.bash
 
 ## Directories that should have the twig lint tool run against them - see php bin/console help lint:twig
-twigDirectories="${projectRoot}/templates"
+twigDirectories=()
+twigDirectories+=("${projectRoot}/templates")
 
 ## Directories that should have the yaml lint tool run against them - see php bin/console help lint:yaml
-yamlDirectories="${projectRoot}/config"
+yamlDirectories=()
+yamlDirectories+=("${projectRoot}/config")
