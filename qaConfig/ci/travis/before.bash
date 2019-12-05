3dbb607f1a9eacd08306a83178c5c63ba10b475e
joseph <joseph@joseph>
2019-12-05 14:06:36 +0000

    tweaking travis before hook

--- qaConfig/ci/travis/before.bash
+++ qaConfig/ci/travis/before.bash
@@ -13,13 +13,13 @@ $(hostname) $0 $@
 "
 projectRootPath="${DIR}/../../../"
 
-rm -f composer.lock
-gitBranch=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
-echo "gitBranch is $gitBranch"
-git checkout $gitBranch
+#rm -f composer.lock
+#gitBranch=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+#echo "gitBranch is $gitBranch"
+#git checkout $gitBranch
 cd $projectRootPath
 composer install
-git checkout HEAD composer.lock
+#git checkout HEAD composer.lock
 
 mkdir -p ${projectRootPath}/var/qa/cache && chmod 777 ${projectRootPath}/var/qa/cache
 
