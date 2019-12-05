db5a74cbc098ad90665d985ba41475ef44a1e51a
joseph <joseph@joseph>
2019-12-05 14:12:18 +0000

    tweaking travis before hook

--- qaConfig/ci/travis/before.bash
+++ qaConfig/ci/travis/before.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-cd $DIR;
+readonly DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $DIR
 set -e
 set -u
 set -o pipefail
@@ -13,11 +13,13 @@ $(hostname) $0 $@
 "
 projectRootPath="${DIR}/../../../"
 
-#rm -f composer.lock
+#
 gitBranch=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 echo "gitBranch is $gitBranch"
 #git checkout $gitBranch
 cd $projectRootPath
+cat composer.json
+rm -f composer.lock
 composer install
 #git checkout HEAD composer.lock
 
