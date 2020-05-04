ca00f0f6d12f7ef4f9a777b95921f27687b3b58b
joseph <joseph+marketing@edmondscommerce.co.uk>
2020-05-04 20:55:36 +0100

    cs fixer was running with xdebug, now much faster and doesnt hang if you have something listening for xdebug

--- includes/generic/phpCsFixer.inc.bash
+++ includes/generic/phpCsFixer.inc.bash
@@ -1,7 +1,7 @@
 set -e
 for phpCsPathToCheck in "${pathsToCheck[@]}"; do
   printf "\nRunning PHP CS Fixer For Path:\n - $phpCsPathToCheck\n\n"
-  ./bin/php-cs-fixer \
+  phpNoXdebug -f ./bin/php-cs-fixer -- \
     --config="$phpCsConfigPath" \
     --cache-file="$phpCsCacheFile.$(echo $phpCsPathToCheck | md5sum -)" \
     --allow-risky=yes \
