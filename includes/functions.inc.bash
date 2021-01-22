2889aa02cfc45bdcc9869f0f3d69d375115a4ffe
Joseph Edmonds <php-book@php-book.local>
2021-01-22 20:47:59 +0000

    git uncommited check can now be skipped interaticely, PHPCS is now disabled as current stable version does not handle PHP8

--- includes/functions.inc.bash
+++ includes/functions.inc.bash
@@ -5,33 +5,29 @@ readonly platformGeneric="generic"
 readonly platformLaravel="laravellumen"
 readonly platformSymfony="symfony"
 
-function detectPlatform(){
+function detectPlatform() {
 
-    if [[ -f $projectRoot/$specifiedPath/etc/module.xml && -f $projectRoot/$specifiedPath/registration.php ]]
-    then
-        echo $platformMagento2
-        return 0
-    fi
+  if [[ -f $projectRoot/$specifiedPath/etc/module.xml && -f $projectRoot/$specifiedPath/registration.php ]]; then
+    echo $platformMagento2
+    return 0
+  fi
 
-    if [[ -f $projectRoot/bin/magento ]]
-    then
-        echo $platformMagento2
-        return 0
-    fi
+  if [[ -f $projectRoot/bin/magento ]]; then
+    echo $platformMagento2
+    return 0
+  fi
 
-    if [[ -f $projectRoot/artisan ]]
-    then
-        echo $platformLaravel;
-        return 0;
-    fi
+  if [[ -f $projectRoot/artisan ]]; then
+    echo $platformLaravel
+    return 0
+  fi
 
-    if [[ -f $projectRoot/symfony.lock ]]
-    then
-        echo $platformSymfony;
-        return 0;
-    fi
+  if [[ -f $projectRoot/symfony.lock ]]; then
+    echo $platformSymfony
+    return 0
+  fi
 
-    echo $platformGeneric
+  echo $platformGeneric
 }
 
 ################################################################
@@ -39,43 +35,40 @@ function detectPlatform(){
 # First check for a project qaConfig tool override
 # Then a platform tool
 # finally the Generic tool
-function runTool(){
-    local tool="$1"
-    local projectOverridePath="$projectConfigPath/tools/$tool.inc.bash"
-    local platformPath="$DIR/../includes/$platform/$tool.inc.bash"
-    local genericPath="$DIR/../includes/generic/$tool.inc.bash"
+function runTool() {
+  local tool="$1"
+  local projectOverridePath="$projectConfigPath/tools/$tool.inc.bash"
+  local platformPath="$DIR/../includes/$platform/$tool.inc.bash"
+  local genericPath="$DIR/../includes/generic/$tool.inc.bash"
 
-    if [[ -f "$projectOverridePath" ]]
-    then
-        echo "Running Project Override $tool"
-        source "$projectOverridePath"
-        return 0
-    elif [[ -f "$platformPath" ]]
-    then
-        echo "Running $platform $tool"
-        source "$platformPath"
-        return 0
-    fi
-    echo "Running generic $tool"
-    source "$genericPath"
+  if [[ -f "$projectOverridePath" ]]; then
+    echo "Running Project Override $tool"
+    source "$projectOverridePath"
+    return 0
+  elif [[ -f "$platformPath" ]]; then
+    echo "Running $platform $tool"
+    source "$platformPath"
+    return 0
+  fi
+  echo "Running generic $tool"
+  source "$genericPath"
 }
 
 # This is very much like run tool
 # It will check for a project level override
 # Then it will run the generic tool explicitly
 function runNonPlatformTool() {
-    local tool="$1"
-    local projectOverridePath="$projectConfigPath/tools/$tool.inc.bash"
-    local genericPath="$DIR/../includes/generic/$tool.inc.bash"
+  local tool="$1"
+  local projectOverridePath="$projectConfigPath/tools/$tool.inc.bash"
+  local genericPath="$DIR/../includes/generic/$tool.inc.bash"
 
-    if [[ -f "$projectOverridePath" ]]
-    then
-        echo "Running Project Override $tool"
-        source "$projectOverridePath"
-        return 0
-    fi
-    echo "Running generic $tool"
-    source "$genericPath"
+  if [[ -f "$projectOverridePath" ]]; then
+    echo "Running Project Override $tool"
+    source "$projectOverridePath"
+    return 0
+  fi
+  echo "Running generic $tool"
+  source "$genericPath"
 }
 
 ################################################################
@@ -88,37 +81,34 @@ function runNonPlatformTool() {
 # Usage:
 #
 # `configPath "relative/path/to/file/or/folder"
-function configPath(){
-    local relativePath="$1"
-    local platformPath="$defaultConfigPath/$platform/$relativePath"
-    local genericPath="$defaultConfigPath/generic/$relativePath"
-    if [[ -f $projectConfigPath/$relativePath ]]
-    then
-        echo $projectConfigPath/$relativePath
-    elif [[ -f $platformPath ]]
-    then
-        echo $platformPath
-    else
-        echo $genericPath
-    fi
+function configPath() {
+  local relativePath="$1"
+  local platformPath="$defaultConfigPath/$platform/$relativePath"
+  local genericPath="$defaultConfigPath/generic/$relativePath"
+  if [[ -f $projectConfigPath/$relativePath ]]; then
+    echo $projectConfigPath/$relativePath
+  elif [[ -f $platformPath ]]; then
+    echo $platformPath
+  else
+    echo $genericPath
+  fi
 }
 
 ###############################################################
 # Function to run PHP without Xdebug enabled, much faster
 # Usage:
 # `phpNoXdebug path/to/php/file.php -- -arg1 -arg2`
-function phpNoXdebug {
-    if [[ ! -f ${noXdebugConfigPath} ]]
-    then
-        # Using awk to ensure that files ending without newlines do not lead to configuration error
-        ${phpBinPath} -i | grep "\.ini" | grep -o -e '\(/[a-z0-9._-]\+\)\+\.ini' | grep -v xdebug | xargs awk 'FNR==1{print ""}1' > "$noXdebugConfigPath"
-    fi
-    set -x
-    ${phpBinPath} -n -c "$noXdebugConfigPath" "$@"
-    local exitCode=$?
-    set +x
-    echo
-    return $exitCode
+function phpNoXdebug() {
+  if [[ ! -f ${noXdebugConfigPath} ]]; then
+    # Using awk to ensure that files ending without newlines do not lead to configuration error
+    ${phpBinPath} -i | grep "\.ini" | grep -o -e '\(/[a-z0-9._-]\+\)\+\.ini' | grep -v xdebug | xargs awk 'FNR==1{print ""}1' >"$noXdebugConfigPath"
+  fi
+  set -x
+  ${phpBinPath} -n -c "$noXdebugConfigPath" "$@"
+  local exitCode=$?
+  set +x
+  echo
+  return $exitCode
 }
 
 ###############################################################
@@ -128,40 +118,35 @@ function phpNoXdebug {
 #
 # Usage:
 # checkForUncommitedChanges
-function checkForUncommittedChanges {
-    if [[ "false" != "${CI:-'false'}" ]]
-    then
-        echo "Skipping uncommited changes check in CI"
-        return 0;
-    fi
-    if [[ "$skipUncommittedChangesCheck" == "1" ]]
-    then
-        echo "Skipping uncommitted changes check. export skipUncommittedChangesCheck=0 to reinstate"
-        return 0
-    fi
+function checkForUncommittedChanges() {
+  if [[ "false" != "${CI:-'false'}" ]]; then
+    echo "Skipping uncommited changes check in CI"
+    return 0
+  fi
+  if [[ "$skipUncommittedChangesCheck" == "1" ]]; then
+    echo "Skipping uncommitted changes check. export skipUncommittedChangesCheck=0 to reinstate"
+    return 0
+  fi
 
-    targetDir=${1:-$(pwd)}
-    originalDir=$(pwd)
+  targetDir=${1:-$(pwd)}
+  originalDir=$(pwd)
 
-    if [[ ! -d $targetDir/.git/ ]]
-    then
-        echo "$targetDir is not a git repo"
-        return
-    fi
+  if [[ ! -d $targetDir/.git/ ]]; then
+    echo "$targetDir is not a git repo"
+    return
+  fi
 
-    cd $targetDir
+  cd $targetDir
 
-    set +e
-    inGitRepo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
-    if [[ "" != "$inGitRepo" ]]
-    then
-        git status | grep -Eq "nothing to commit, working .*? clean"
-        repoDirty=$?
-        set -e
-        if (( $repoDirty > 0 ))
-        then
-            git status
-            echo "
+  set +e
+  inGitRepo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+  if [[ "" != "$inGitRepo" ]]; then
+    git status | grep -Eq "nothing to commit, working .*? clean"
+    repoDirty=$?
+    set -e
+    if (($repoDirty > 0)); then
+      git status
+      echo "
 
     ==================================================
 
@@ -171,39 +156,48 @@ function checkForUncommittedChanges {
 
         (git commit will be 'git add -A; git commit')
 
-        or export skipUncommittedChangesCheck=1 to ignore
+        Alternatively you can skip (s), but please be aware that from this point on, your code is going to be
+        actively changed and not having a git commit to restore to could cause you significant pain.
+        You have been warned.
+
+        To always skip, you can export skipUncommittedChangesCheck=1
 
     ==================================================
 
             "
-            read -n 1 commitOrAbort
-            if [[ "$commitOrAbort" != "c" ]]
-            then
-                printf "\n\n\nAborting...\n\n\n"
-                exit 1
-            fi
-            git add -A
-            git commit
-        fi
+      read -n 1 commitOrAbort
+      case "$commitOrAbort" in
+      s)
+        echo "Skipping"
+        return 0
+        ;;
+      c)
+        git add -A
+        git commit
+        ;;
+      *)
+        printf "\n\n\nAborting...\n\n\n"
+        exit 1
+        ;;
+      esac
     fi
+  fi
 
-    cd $originalDir
+  cd $originalDir
 }
 
-function phpunitReRunFailedOrFull(){
-    if [[ "false" != "${CI:-'false'}" ]]
-    then
-        return 0;
-    fi
-    local rerunFailed
-    local reunLogFileTimeLimit=${phpunitRerunTimeoutMins:-5}
-    local rerunLogFile="$(find $varDir -type f -name 'phpunit.junit.xml' -mmin -$reunLogFileTimeLimit)";
-    if [[ "" == "$rerunLogFile" ]]
-    then
-        echo "";
-        return 1;
-    fi
-    echo "
+function phpunitReRunFailedOrFull() {
+  if [[ "false" != "${CI:-'false'}" ]]; then
+    return 0
+  fi
+  local rerunFailed
+  local reunLogFileTimeLimit=${phpunitRerunTimeoutMins:-5}
+  local rerunLogFile="$(find $varDir -type f -name 'phpunit.junit.xml' -mmin -$reunLogFileTimeLimit)"
+  if [[ "" == "$rerunLogFile" ]]; then
+    echo ""
+    return 1
+  fi
+  echo "
 
     ==================================================
 
@@ -216,24 +210,21 @@ function phpunitReRunFailedOrFull(){
     ==================================================
 
         "
-    set +e
-    read  -t 10 -n 1 rerunFailed
-    set -e
-    if [[ "y" != "$rerunFailed" ]]
-    then
-        printf "\n\nRunning Full...\n\n"
-        return 1
-    fi
-    printf "\n\nRerunning Failed Only\n\n"
-    return 0;
+  set +e
+  read -t 10 -n 1 rerunFailed
+  set -e
+  if [[ "y" != "$rerunFailed" ]]; then
+    printf "\n\nRunning Full...\n\n"
+    return 1
+  fi
+  printf "\n\nRerunning Failed Only\n\n"
+  return 0
 }
 
-
-function tryAgainOrAbort(){
-    toolname="$1"
-    if [[ "false" != "${CI:-'false'}" ]]
-    then
-        echo "
+function tryAgainOrAbort() {
+  toolname="$1"
+  if [[ "false" != "${CI:-'false'}" ]]; then
+    echo "
 
     ==================================================
 
@@ -242,9 +233,9 @@ function tryAgainOrAbort(){
     ==================================================
 
         "
-        exit 1
-    fi
-    echo "
+    exit 1
+  fi
+  echo "
 
     ==================================================
 
@@ -257,28 +248,24 @@ function tryAgainOrAbort(){
     ==================================================
 
     "
-    while read -n 1 tryAgainOrAbort
-    do
-        if [[ "n" == "$tryAgainOrAbort" ]]
-        then
-            printf "\n\nAborting...\n\n"
-            exit 1
-        fi
-        if [[ "y" == "$tryAgainOrAbort" ]]
-        then
-            break;
-        fi
-        printf "\n\ninvalid choice: $tryAgainOrAbort - should be y or n \n\n        would you like to try again? (y/n)"
-    done
-    printf "\n\nTrying again, good luck!\n\n"
-    hasBeenRestarted="true"
+  while read -n 1 tryAgainOrAbort; do
+    if [[ "n" == "$tryAgainOrAbort" ]]; then
+      printf "\n\nAborting...\n\n"
+      exit 1
+    fi
+    if [[ "y" == "$tryAgainOrAbort" ]]; then
+      break
+    fi
+    printf "\n\ninvalid choice: $tryAgainOrAbort - should be y or n \n\n        would you like to try again? (y/n)"
+  done
+  printf "\n\nTrying again, good luck!\n\n"
+  hasBeenRestarted="true"
 }
 
-function findTestsDir(){
-    testsDir="$(find $projectRoot -maxdepth 1 -type d \( -name test -o -name tests \) | head -n1)"
-    if [[ "" == "$testsDir" ]]
-    then
-        echo "
+function findTestsDir() {
+  testsDir="$(find $projectRoot -maxdepth 1 -type d \( -name test -o -name tests \) | head -n1)"
+  if [[ "" == "$testsDir" ]]; then
+    echo "
 
 
     ##### ERROR ############################################
@@ -295,18 +282,16 @@ function findTestsDir(){
 
     ########################################################
 
-        " 1>&2;
-        exit 1
-    fi
-    echo "$testsDir"
+        " 1>&2
+    exit 1
+  fi
+  echo "$testsDir"
 }
 
-
-function findSrcDir(){
-    srcDir="$projectRoot/src"
-    if [[ "" == "$srcDir" ]]
-    then
-        echo "
+function findSrcDir() {
+  srcDir="$projectRoot/src"
+  if [[ "" == "$srcDir" ]]; then
+    echo "
 
 
     ##### ERROR ############################################
@@ -323,17 +308,16 @@ function findSrcDir(){
 
     ########################################################
 
-        " 1>&2;
-        exit 1
-    fi
-    echo "$srcDir"
+        " 1>&2
+    exit 1
+  fi
+  echo "$srcDir"
 }
 
-function findBinDir(){
-    binDir="$(find $projectRoot -maxdepth 2 -type d -name bin | head -n1)"
-    if [[ "" == "$binDir" ]]
-    then
-        echo "
+function findBinDir() {
+  binDir="$(find $projectRoot -maxdepth 2 -type d -name bin | head -n1)"
+  if [[ "" == "$binDir" ]]; then
+    echo "
 
 
     ##### ERROR ############################################
@@ -350,8 +334,8 @@ function findBinDir(){
 
     ########################################################
 
-        " 1>&2;
-        exit 1
-    fi
-    echo "$binDir"
+        " 1>&2
+    exit 1
+  fi
+  echo "$binDir"
 }
--- includes/generic/allCodingStandardsTools.inc.bash
+++ includes/generic/allCodingStandardsTools.inc.bash
@@ -6,18 +6,21 @@ Running PHP-CS-Fixer
 
 runTool phpCsFixer
 
-echo "
-
-Running Beautifier and Fixer
-----------------------------
-"
-
-runTool beautifierFixer
-
-echo "
-
-Running Code Sniffer
---------------------
-"
-
-runTool codeSniffer
+# Code Sniffer does not currently support PHP8, for example
+# https://github.com/squizlabs/PHP_CodeSniffer/issues/3167
+# Hopefully 3.6 release will fix this, but for now lets disable the whole thing because it's totally broken
+#echo "
+#
+#Running Beautifier and Fixer
+#----------------------------
+#"
+#
+#runTool beautifierFixer
+#
+#echo "
+#
+#Running Code Sniffer
+#--------------------
+#"
+#
+#runTool codeSniffer
