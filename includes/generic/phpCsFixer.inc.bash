d1e00caf95bebe4d02966806cf8c3dfb5717057f
Joseph Edmonds <php-book@php-book.local>
2021-01-22 19:50:11 +0000

    retry or abort loop for cs fixer

--- includes/generic/phpCsFixer.inc.bash
+++ includes/generic/phpCsFixer.inc.bash
@@ -1,11 +1,21 @@
 set -e
 for phpCsPathToCheck in "${pathsToCheck[@]}"; do
   printf "\nRunning PHP CS Fixer For Path:\n - $phpCsPathToCheck\n\n"
-  phpNoXdebug -f ./bin/php-cs-fixer -- \
-    --config="$phpCsConfigPath" \
-    --cache-file="$phpCsCacheFile.$(echo $phpCsPathToCheck | md5sum -)" \
-    --allow-risky=yes \
-    --show-progress=dots \
-    fix \
-    $phpCsPathToCheck
+
+  csFixerExitCode=99
+  while ((csFixerExitCode > 1)); do
+    set +e
+    phpNoXdebug -f ./bin/php-cs-fixer -- \
+      --config="$phpCsConfigPath" \
+      --cache-file="$phpCsCacheFile.$(echo $phpCsPathToCheck | md5sum -)" \
+      --allow-risky=yes \
+      --show-progress=dots \
+      fix \
+      $phpCsPathToCheck
+    csFixerExitCode=$?
+    set -e
+    if ((csFixerExitCode > 0)); then
+      tryAgainOrAbort "PHP CS Fixer"
+    fi
+  done
 done
